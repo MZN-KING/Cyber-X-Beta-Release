e799f03a9fabfb4cb6de908012e8451ba643e291
Mazeen Ahamed <106764948+MZN-KING@users.noreply.github.com>
2023-06-11 21:05:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,26 +36,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94789481495'] //ur owner number
+global.ownername = "𝙼𝚁 𝙼𝙰𝙰𝚉𝙸𝙽 𝙺𝙸𝙽𝙶" //ur owner name
+global.ytname = "MAAZIN KING" //ur yt chanel name
+global.socialm = "GitHub: MznStudiosOfc" //ur github or insta name
+global.location = "Matara, Sri Lanka" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['94789481495']
+global.ownertag = '94789481495' //ur tag number
+global.botnumber = '94789481495' //Add your Bot Number For Log
+global.botname = '『 𝙼𝚉𝙽 𝙱𝙾𝚃𝚂 』• 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻' //ur bot name
+global.ig = "https://wa.me/94789481495" //your theme url which will be displayed on whatsapp
+global.myweb = "https://wa.me/94789481495" //ur website to be displayed
+global.sc = 'https://wa.me/94789481495' //script link
+global.linkz = 'https://wa.me/94789481495'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "『 𝙼𝚉𝙽 𝙱𝙾𝚃𝚂 』• 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻" //ur sticker watermark packname
+global.author = "𝙼𝚁 𝙼𝙰𝙰𝚉𝙸𝙽 𝙺𝙸𝙽𝙶" //ur sticker watermark author
+global.footer = "© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙼𝚉𝙽 𝙱𝙾𝚃𝚂²⁰²³"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94789481495'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
